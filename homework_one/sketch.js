59f5409d16df56ff614a7cf0132fb6aab9d8d5dc
janicechen19 <31458052+janicechen19@users.noreply.github.com>
2017-09-05 19:23:17 -0400

    Initial Upload: Week One Homework

---
+++ homework_one/sketch.js
@@ -0,0 +1,63 @@
+function setup() {
+    createCanvas(800,500);
+}
+
+function draw() {
+    background(191,245,255);
+    noStroke();
+    fill(255);
+    /*Snowman Body*/
+    ellipse(646,413,174,174);
+    ellipse(646,281,129,129);
+    ellipse(646,180,102,102);
+    rect(400,469,400,48);
+    /*Snow Pile*/
+    ellipse(389,473,97,97);
+    ellipse(299,420,97,97);
+    ellipse(322,469,97,97);
+    ellipse(253,478,97,97);
+    ellipse(224,433,97,97);
+    ellipse(181,492,97,97);
+    ellipse(199,374,97,97);
+    ellipse(126,392,97,97);
+    ellipse(150,429,97,97);
+    ellipse(101,474,97,97);
+    ellipse(78,440,97,97);
+    ellipse(34,468,97,97);
+    ellipse(257,385,97,97);
+    ellipse(47,420,97,97);
+    /*Snowflakes*/
+    ellipse(43,106,17,17);
+    ellipse(60,254,17,17);
+    ellipse(87,177,17,17);
+    ellipse(138,98,17,17);
+    ellipse(163,18,17,17);
+    ellipse(150,231,17,17);
+    ellipse(207,151,17,17);
+    ellipse(295,72,17,17);
+    ellipse(339,159,17,17);
+    ellipse(322,246,17,17);
+    ellipse(416,122,17,17);
+    ellipse(417,327,17,17);
+    ellipse(482,229,17,17);
+    ellipse(511,372,17,17);
+    ellipse(533,159,17,17);
+    ellipse(629,21,17,17);
+    ellipse(748,151,17,17);
+    ellipse(765,309,17,17);
+    
+    /*Snowman's Features*/
+    fill(0);
+    rect(606,87,80,53);
+    rect(596,136,100,15);
+    ellipse(629,171,13,13);
+    ellipse(665,171,13,13);
+    ellipse(620,203,13,13);
+    ellipse(672,203,13,13);
+    ellipse(635,216,13,13);
+    ellipse(655,216,13,13);
+    
+    fill(247,147,30);
+    ellipse(646,190,13,13);
+    
+}
